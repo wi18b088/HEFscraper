8253dd11e6cce979745577847448420046098113
Albert <wi18b088@technikum-wien.at>
2020-10-28 11:11:08 +0100

    work with offline csv

--- src/springerlink.py
+++ src/springerlink.py
@@ -4,5 +4,13 @@ import pandas as pd
 keywords = ["hybrid", "electric", "flying", "aircraft"]
 
 # Get CSV from springerlink search (nice feature provided by Springer Link)
-df = pd.read_csv(f'https://link.springer.com/search/csv?showAll=false&query={"+".join(keywords)}')
-print(df)
+# URL taken from https://link.springer.com/search
+# Additional constraints possible
+# Download from web disabled for development / debug
+# df = pd.read_csv(f'https://link.springer.com/search/csv?showAll=false&query={"+".join(keywords)}')
+
+df = pd.read_csv('src/springerlink_search_results.csv')
+
+# Print information about dataframe
+print(df.columns)                       # Prints the headers
+print(df["Item Title"][0:5])            # Prints the first 5 titles of available papers
